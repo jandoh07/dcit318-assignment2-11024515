dba6341c8618cdf66c78dd395dd818ef917ce99d
jandoh07 <150073132+jandoh07@users.noreply.github.com>
2025-08-10 14:30:25 +0000

    Add Inheritance and method overriding

---
+++ Inheritance/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace dcit318_assignment2_11024515.Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal = new Animal();
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+
+            animal.MakeSound();
+            dog.MakeSound();
+            cat.MakeSound();
+        }
+    }
+
+    public class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic sound");
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    public class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+}
