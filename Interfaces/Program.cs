0c019629f00fcc55650d1b95e2fc0fc704b03940
jandoh07 <150073132+jandoh07@users.noreply.github.com>
2025-08-10 14:41:32 +0000

    Add interfaces

---
+++ Interfaces/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace dcit318_assignment2_11024515.Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            Bicycle bicycle = new Bicycle();
+
+            car.Move();
+            bicycle.Move();
+        }
+    }
+
+    public interface IMovable
+    {
+        void Move();
+    }
+
+    public class Car : IMovable
+    {
+        public void Move() => Console.WriteLine("Car is moving");
+    }
+
+    public class Bicycle : IMovable
+    {
+        public void Move() => Console.WriteLine("Bicycle is moving");
+    }
+
+}
